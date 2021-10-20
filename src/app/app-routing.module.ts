24d085b023b47d76e1fb9066b782326bdbab4d93
Malcolm Camelo <217137334@mycput.ac.za>
2021-10-20 03:22:22 +0200

    create member almost done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateuserComponent } from './createuser/createuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component: CreateuserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
