8687cf3670d9206e6ae777b96e2b436805b3a09f
EmilioCastano21 <219035709@mycput.ac.za>
2021-10-20 12:35:32 +0200

    Added create movie component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreatemovieComponent} from "./createmovie/createmovie.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: CreatemovieComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
