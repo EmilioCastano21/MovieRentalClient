affc18da702cb75ff5ec494b119e819ce0a75391
kccra <kccrafford@gmail.com>
2021-10-20 16:37:02 +0200

    219169640: Completed create movie

--- src/app/createmovie/createmovie.component.ts
+++ src/app/createmovie/createmovie.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Movie } from '../entity/movie';
+import { MovieService } from '../service/movie.service';
 
 @Component({
   selector: 'app-createmovie',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createmovie.component.css']
 })
 export class CreatemovieComponent implements OnInit {
-
-  constructor() { }
+  public movies: Movie[] = [];
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
   }
 
+  public getMovies():void {
+    this.movieService.getMovies().subscribe(
+      (response: Movie[]) => {
+        this.movies = response;
+      },
+      (error: HttpErrorResponse)=> {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddMovie(addForm:NgForm): void{
+      this.movieService.addMovie(addForm.value).subscribe(
+        (response: Movie)=>{
+          console.log(response);
+          this.getMovies();
+          alert("saved");
+        },
+        (error: HttpErrorResponse)=>{
+          alert(error.message);
+        }
+      );
+  }
 }
--- src/app/createuser/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User{
-  userId : string;
-  address : string;
-  email : string;
-  cellNumber : string;
-  idNumber : string;
-  firstName : string;
-  lastName : string;
-}
